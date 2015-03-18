24b8d0328d073eb43df15882972b49e5ad8d4898
coolguazi <aguazi1990@yahoo.com.tw>
2015-03-18 14:14:24 +0800

    math

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int main()
 {
 	printf("Hello World!\n");
     printf("%d\n" , 2+3);
 	printf("%f\n" , 1032+2.33);
+	printf("%.16f\n" , sin(2*M_PI/3));
 	return 123;
 }
 
