e31529077fea81460c915be8be3fcc2ba4454718
coolguazi <aguazi1990@yahoo.com.tw>
2015-03-18 14:18:57 +0800

    print a name

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-
 int main()
 {
+	char Name[50]="Ming-Huan Tsai";
 	printf("Hello World!\n");
-    printf("%d\n" , 2+3);
-	printf("%f\n" , 1032+2.33);
+    printf("%d\n", 2+3);
+	printf("%f\n", 1032+2.33);
+	printf("%s\n", Name);
 	return 123;
 }
 
